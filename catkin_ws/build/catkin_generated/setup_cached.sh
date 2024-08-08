32ddac42c4e975a649e2888ad248a9a4a456d486
taehun-kmu <taehun@kookmin.ac.kr>
2024-08-08 15:45:23 +0200

    fix

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,4 +8,4 @@
 
 # modified environment variables
 export PWD='/home/jetson/2024-Kudos-Hackthon/catkin_ws/build'
-export PYTHONPATH='/home/jetson/2024-Kudos-Hackthon/catkin_ws/devel/lib/python3/dist-packages:/home/jetson/2024-Kudos-Hackthon/darknet_ws/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:/home/jetson/ros_ws/darknet_ws/devel/lib/python2.7/dist-packages'
+export PYTHONPATH='/home/jetson/2024-Kudos-Hackthon/catkin_ws/devel/lib/python3/dist-packages:/home/jetson/2024-Kudos-Hackthon/darknet_ws/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages'
--- catkin_ws/build/yolo_v3_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/2024-Kudos-Hackthon/catkin_ws/src/yolo_v3_node
-# Build directory: /home/jetson/2024-Kudos-Hackthon/catkin_ws/build/yolo_v3_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
